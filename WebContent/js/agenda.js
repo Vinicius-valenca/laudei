f3ffb98f85b94aa25038a83e167a093164074516
Vinicius Valença <contato@laudeitelemedicina.com.br>
2021-05-21 14:49:40 -0300

    nova versao15

--- WebContent/js/agenda.js
+++ WebContent/js/agenda.js
@@ -31,7 +31,9 @@
 									  this.reset();
 									});
 					$("#id").val(row.id);
+					$('#nomeLaudo').remove();
 					$("#idPessoa").val($("#idPessoa1").val());
+					$('#nomeLaudo').append("Laudo - ");
 					$('#nomeLaudo').append(" - "+row.tpExame);
 					$('#nomeLaudo').append(" - "+row.nomePaciente);
 					$("#examenome").val(row.examenome);
