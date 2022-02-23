a9079e4e07953b5a3799a94a5b2a3536111a8cda
Vinicius Valença <vinicius.valenca@outlook.com.br>
2022-02-23 09:19:22 -0300

    fix dbv4

--- src/hibernate/util/HibernateUtil.java
+++ src/hibernate/util/HibernateUtil.java
@@ -7,7 +7,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
