fd7b02ce4b3148752afbe77e9845bc985008a523
Vinicius Valença <vinicius.valenca@outlook.com.br>
2022-02-23 13:10:34 -0300

    fix dbv5

--- src/controller/UsuarioFilter.java
+++ src/controller/UsuarioFilter.java
@@ -9,18 +9,36 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Pessoa;
 
 //@WebFilter(urlPatterns = { "/*" })
 public class UsuarioFilter implements Filter {
+	
+	private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+	  private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+	  private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+	  private static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+	  private static final String ALL = "*";
+	  private static final String ALLOWED_METHODS = "GET, POST";
+	  private static final String ALLOWED_HEADERS =
+	      "origin, content-type, accept, Authorization, x-requested-with";
+	  private static final String EXPOSE_HEADERS = "TEST";
 
 	public void destroy() {
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
+		 HttpServletResponse sresponse = (HttpServletResponse) response;
+		    sresponse.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, ALL);
+		    sresponse.setHeader(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
+		    sresponse.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
+		    sresponse.setHeader(ACCESS_CONTROL_EXPOSE_HEADERS, EXPOSE_HEADERS);
+		    
 
 		String path = ((HttpServletRequest) request).getRequestURI();
 		
@@ -51,5 +69,7 @@ public class UsuarioFilter implements Filter {
 
 	public void init(FilterConfig fConfig) throws ServletException {
 	}
+	
+	
 
 }
